e0011e6d6cc0dbf9a1aa4c5c0d729bfaa589ec69
Joel Rainear Wills <joel@teamsoftware.com>
2020-03-29 15:54:54 -0500

    fixing folder names

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,56 +1,15 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import { Jobs } from '../jobs';
+import { Landing } from '../Landing';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper
-  }
-}));
+const theme = createMuiTheme();
 
-const App = () => {
-  const classes = useStyles();
-  const [tabIndex, setTabIndex] = React.useState(0);
-
-  const handleChange = (event, newIndex) => {
-    setTabIndex(newIndex);
-  };
-
+export const App = () => {
   return (
-    <div className={classes.root}>
-      <AppBar position='static'>
-        <Tabs
-          value={tabIndex}
-          onChange={handleChange}
-          aria-label='Cash Flow Navigation'>
-          <Tab label='Jobs' aria-controls='jobs-tab' />
-          <Tab label='Clients' aria-controls='clients-tab' />
-        </Tabs>
-      </AppBar>
-      <Typography
-        component='div'
-        role='tabpanel'
-        hidden={tabIndex !== 0}
-        id={`jobs-tab`}
-        aria-labelledby={`jobs-tab`}>
-        {tabIndex === 0 && <Jobs></Jobs>}
-      </Typography>
-      <Typography
-        component='div'
-        role='tabpanel'
-        hidden={tabIndex !== 1}
-        id={`clients-tab`}
-        aria-labelledby={`clients-tab`}>
-        {tabIndex === 1 && <Box p={3}>Jobs</Box>}
-      </Typography>      
-    </div>
+    <ThemeProvider theme={theme}>
+      <Landing />
+    </ThemeProvider>
   );
 };
 
