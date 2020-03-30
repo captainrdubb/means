25d35792db1c5b94dc53f6cc21d41808a08dd719
Joel Rainear Wills <joel@teamsoftware.com>
2020-03-29 23:59:14 -0500

    using rxjs

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Landing } from '../landing';
 
 const theme = createMuiTheme();
@@ -8,7 +9,9 @@ const theme = createMuiTheme();
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Landing />
+      <Router>
+        <Landing />
+      </Router>
     </ThemeProvider>
   );
 };
--- src/landing/Landing.jsx
+++ src/landing/Landing.jsx
@@ -18,6 +18,8 @@ import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { Route, Switch, Redirect } from 'react-router';
+import { Jobs } from '../jobs';
 
 const drawerWidth = 240;
 
@@ -86,7 +88,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function Landing() {
+const Landing = ({ location }) => {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
@@ -130,13 +132,15 @@ export default function Landing() {
           paper: classes.drawerPaper
         }}>
         <div className={classes.drawerHeader}>
-          {!isAlwaysOpen && <IconButton onClick={toggleDrawer}>
-            {theme.direction === 'ltr' ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>}
+          {!isAlwaysOpen && (
+            <IconButton onClick={toggleDrawer}>
+              {theme.direction === 'ltr' ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          )}
         </div>
         <Divider />
         <List>
@@ -166,36 +170,14 @@ export default function Landing() {
           [classes.contentShift]: open
         })}>
         <div className={classes.drawerHeader} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Switch>
+          <Route exact path='/'>
+            <Redirect to='/jobs' />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
-}
+};
+
+export default Landing;
