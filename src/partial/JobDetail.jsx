27b23e5926fb0be49ed98f8967ae164fb248a6a7
Joel Rainear Wills <joel@teamsoftware.com>
2020-04-02 17:53:46 -0500

    removed unused style

--- src/partial/JobDetail.jsx
+++ src/partial/JobDetail.jsx
@@ -17,11 +17,7 @@ import {
   selectJob
 } from '../state';
 
-const useStyles = makeStyles((theme) => ({
-  client: {
-    width: 250
-  }
-}));
+const useStyles = makeStyles((theme) => ({}));
 
 const JobDetail = () => {
   const classes = useStyles();
@@ -44,22 +40,77 @@ const JobDetail = () => {
   };
 
   return (
-    <Grid container spacing={1} justify='center' direction='column'>
-      <Grid item>
-        <TextField id='' label='Title' value={jobForm.title} />
-      </Grid>
-      <Grid item>
-        <Autocomplete
-          id=''
-          options={clients}
-          onChange={onClientChange}
-          className={classes.client}
-          defaultValue={jobForm.client}
-          getOptionLabel={(client) => `${client.firstName} ${client.lastName}`}
-          renderInput={(params) => (
-            <TextField {...params} label='Client' variant='standard' />
-          )}
-        />
+    <Grid container>
+      <Grid container spacing={3} xs={12} md={5}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            id=''
+            label='Description'
+            value={jobForm.title}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Autocomplete
+            id=''
+            fullWidth
+            options={clients}
+            onChange={onClientChange}
+            className={classes.client}
+            defaultValue={jobForm.client}
+            getOptionLabel={(client) =>
+              `${client.firstName} ${client.lastName}`
+            }
+            renderInput={(params) => (
+              <TextField {...params} label='Client' variant='standard' />
+            )}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id=''
+            label='Address line 1'
+            autoComplete='address-line1'
+            value={jobForm.location.addressOne}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id=''
+            label='Address line 2'
+            autoComplete='address-line2'
+            value={jobForm.location.addressTwo}
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id=''
+            label='City'
+            autoComplete='address-level2'
+            value={jobForm.location.city}
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id=''
+            label='State'
+            autoComplete='address-level1'
+            value={jobForm.location.state}
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id=''
+            label='Zip'
+            autoComplete='postal-code'
+            value={jobForm.location.zip}
+            fullWidth
+          />
+        </Grid>
       </Grid>
     </Grid>
   );
