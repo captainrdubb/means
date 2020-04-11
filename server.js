a06cbcfa6e0e451b52a61dfa7b114b954e5d5f13
Joel Rainear Wills <joel@teamsoftware.com>
2020-04-11 16:27:02 -0500

    build the app bro

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.static(public));
 
 app.get('/', (req, res) => res.sendFile(path.join(public, 'index.html')));
 
-let options = {};
 if (process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'dev') {
   https
     .createServer(
